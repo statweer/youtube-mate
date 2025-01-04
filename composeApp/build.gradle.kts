0a8e3297c15699bff64bdac4e8c273b56514c4b4
Reza Moallemi <rm.allmails@gmail.com>
2025-01-04 09:44:44 -0800

    Add Android App Target

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,17 +1,32 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
-  alias(libs.plugins.jetbrainsCompose)
-  alias(libs.plugins.compose.compiler)
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.composeMultiplatform)
+  alias(libs.plugins.composeCompiler)
   alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
+  androidTarget {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_11)
+    }
+  }
+
   jvm("desktop")
 
   sourceSets {
     val desktopMain by getting
+
+    androidMain.dependencies {
+      implementation(compose.preview)
+      implementation(libs.androidx.activity.compose)
+    }
 
     commonMain.dependencies {
       implementation(compose.runtime)
@@ -33,6 +48,7 @@ kotlin {
 
       implementation(libs.ktor.client.core)
       implementation(libs.ktor.client.logging)
+      implementation(libs.ktor.client.okhttp)
       implementation(libs.ktor.client.content.negotation)
       implementation(libs.ktor.client.kotlinx.serialization)
 
@@ -42,10 +58,41 @@ kotlin {
     desktopMain.dependencies {
       implementation(compose.desktop.currentOs)
       implementation(libs.kotlinx.coroutines.swing)
-
-      implementation(libs.ktor.client.okhttp)
     }
   }
+}
+
+android {
+  namespace = "me.moallemi.youtubemate"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+  defaultConfig {
+    applicationId = "me.moallemi.youtubemate"
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+    versionCode = 1
+    versionName = "1.0"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+      excludes += "/META-INF/INDEX.LIST"
+      excludes += "/META-INF/DEPENDENCIES"
+    }
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+}
+
+dependencies {
+  debugImplementation(compose.uiTooling)
 }
 
 compose.desktop {
--- composeApp/src/commonMain/kotlin/me/moallemi/youtubemate/compoentns/TopCommentersSection.kt
+++ composeApp/src/commonMain/kotlin/me/moallemi/youtubemate/compoentns/TopCommentersSection.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
